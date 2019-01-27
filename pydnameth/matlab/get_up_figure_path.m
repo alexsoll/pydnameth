da0dfca2ed8a374f7856828b5aeccfae326df839
Unknown <hewitt.archie@yandex.ru>
2019-01-27 17:39:24 +0300

    Deleted MATLAB scripts, added author, changed doc theme

--- pydnameth/matlab/get_up_figure_path.m
+++
@@ -1,9 +0,0 @@
-function up_save = get_up_figure_path()
-if strcmp(getenv('computername'), 'MSI')
-    up_save = 'C:/Users/user/Google Drive/mlmg/figures';
-elseif strcmp(getenv('computername'), 'DESKTOP-4BEQ7MS')
-    up_save = 'D:/Aaron/Bio/pymethspec/figures';
-else
-    up_save = 'C:/Users/user/Google Drive/mlmg/figures';
-end
-end
--- pydnameth/pydnameth.py
+++ pydnameth/pydnameth.py
@@ -1,3 +1,40 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+from pydnameth.config.config import Config
+from pydnameth.config.data.data import Data
+from pydnameth.config.setup.setup import Setup
+from pydnameth.config.setup.types import Experiment, Task, Method
+
+
+def get_config(data,
+               setup,
+               annotations,
+               attributes,
+               target):
+    config = Config(data, setup, annotations, attributes, target)
+    return config
+
+
+def get_data(name,
+             type,
+             path,
+             base):
+    data = Data(name, type, path, base)
+    return data
+
+
+def get_setup(experiment,
+              task,
+              method,
+              params):
+    if not isinstance(experiment, Experiment):
+        raise ValueError('experiment must be Experiment instance')
+    if not isinstance(task, Task):
+        raise ValueError('task must be Task instance')
+    if not isinstance(method, Method):
+        raise ValueError('method must be Method instance')
+
+    setup = Setup(experiment, task, method, params)
+    return setup
