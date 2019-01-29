7271b4724754600782ff73f3319574a2ce60b77f
Aaron <aaron.blare@mail.ru>
2019-01-29 18:14:29 +0300

    Bump version: 0.1.8 → 0.1.9

--- pydnameth/__init__.py
+++ pydnameth/__init__.py
@@ -5,7 +5,7 @@
 
 __author__ = """Aaron Blare"""
 __email__ = 'aaron.blare@mail.ru'
-__version__ = '0.1.8'
+__version__ = '0.1.9'
 
 from .config.config import Config
 from .config.common import CommonTypes
