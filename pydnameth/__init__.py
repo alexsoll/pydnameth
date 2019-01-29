d332c611dac9fd8c32320a3c50578e5d06bf02c7
Unknown <aaron.blare@mail.ru>
2019-01-29 18:10:59 +0300

    Package encapsulating

--- pydnameth/__init__.py
+++ pydnameth/__init__.py
@@ -7,14 +7,14 @@ __author__ = """Aaron Blare"""
 __email__ = 'aaron.blare@mail.ru'
 __version__ = '0.1.8'
 
-from pydnameth.config.config import Config
-from pydnameth.config.common import CommonTypes
-from pydnameth.config.annotations.annotations import Annotations
-from pydnameth.config.annotations.types import AnnotationKey
-from pydnameth.config.annotations.types import Exclude, CrossReactive, SNP, Chromosome, GeneRegion, Geo, ProbeClass
-from pydnameth.config.attributes.attributes import Cells, Observables, Attributes
-from pydnameth.config.data.data import Data
-from pydnameth.config.data.types import DataPath, DataBase, DataType
-from pydnameth.config.setup.setup import Setup
-from pydnameth.config.setup.types import Experiment, Task, Method
-from pydnameth.model.experiment import base, advanced, plot
+from .config.config import Config
+from .config.common import CommonTypes
+from .config.annotations.annotations import Annotations
+from .config.annotations.types import AnnotationKey
+from .config.annotations.types import Exclude, CrossReactive, SNP, Chromosome, GeneRegion, Geo, ProbeClass
+from .config.attributes.attributes import Cells, Observables, Attributes
+from .config.data.data import Data
+from .config.data.types import DataPath, DataBase, DataType
+from .config.setup.setup import Setup
+from .config.setup.types import Experiment, Task, Method
+from .model.experiment import base, advanced, plot
