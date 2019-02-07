19cd00c7a2d63e10c1c7ccaff5826177f724c9dd
Unknown <aaron.blare@mail.ru>
2019-02-07 18:38:54 +0300

    Fixed docs path

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'), os.path.abspath('..\\pydnameth\\scripts'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(1, os.path.abspath('..\\pydnameth\\scripts'))
 
 import pydnameth
 
