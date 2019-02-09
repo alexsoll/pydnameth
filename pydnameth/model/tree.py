81256361992bf7b1e14d23aea77f5378c798fca1
Unknown <hewitt.archie@yandex.ru>
2019-02-10 00:14:34 +0300

    Fixed hash tree

--- pydnameth/model/tree.py
+++ pydnameth/model/tree.py
@@ -17,9 +17,17 @@ def calc_tree(root):
 def build_tree(root):
 
     for node in PostOrderIter(root):
+
+        node_status = node.config.is_root
+        node.config.is_root = True
+        node.name = str(node.config)
+
         exporter = JsonExporter(sort_keys=True)
         node_json = exporter.export(node).encode('utf-8')
         hash = hashlib.md5(node_json).hexdigest()
         node.config.set_hash(hash)
         if node.config.is_run:
             save_info(node)
+
+        node.config.is_root = node_status
+        node.name = str(node.config)
