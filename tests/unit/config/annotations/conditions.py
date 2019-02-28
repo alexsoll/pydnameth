6aef07b8e1e43443c3e1ee945723f8487a6af046
Nikita Timakin <niktimak@gmail.com>
2019-02-28 19:03:48 +0300

    Test for annotations.conditions snp_condition added

--- tests/unit/config/annotations/conditions.py
+++ tests/unit/config/annotations/conditions.py
@@ -10,6 +10,7 @@ from pydnameth import Config
 from pydnameth.infrastucture.load.excluded import load_excluded
 from pydnameth.config.annotations.types import AnnotationKey
 from pydnameth.config.annotations.conditions import exclude_condition
+from pydnameth.config.annotations.conditions import snp_condition
 
 
 class TestAnnotationsConditions(unittest.TestCase):
@@ -68,6 +69,21 @@ class TestAnnotationsConditions(unittest.TestCase):
         ex_cpg = {AnnotationKey.cpg.value: 'cg00000165'}
         is_not_excluded = exclude_condition(self.config, ex_cpg)
         self.assertEqual(False, is_not_excluded)
+
+    def test_snp_condition(self):
+        annotations_dict = {AnnotationKey.Probe_SNPs.value: 'rs12616624',
+                            AnnotationKey.Probe_SNPs_10.value: 'rs35342923'}
+        condition1 = snp_condition(self.config, annotations_dict)
+
+        annotations_dict = {AnnotationKey.Probe_SNPs.value: '',
+                            AnnotationKey.Probe_SNPs_10.value: 'rs35342923'}
+        condition2 = snp_condition(self.config, annotations_dict)
+
+        annotations_dict = {AnnotationKey.Probe_SNPs.value: 'rs12616624',
+                            AnnotationKey.Probe_SNPs_10.value: ''}
+        condition3 = snp_condition(self.config, annotations_dict)
+
+        self.assertEqual(False, condition1 or condition2 or condition3)
 
 
 if __name__ == '__main__':
