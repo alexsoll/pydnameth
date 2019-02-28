aefff5cabcd722b945aae121242b166483612730
Nikita Timakin <niktimak@gmail.com>
2019-02-28 14:57:51 +0300

    Test for annotations.conditions exclude_condition added

--- tests/unit/config/annotations/conditions.py
+++ tests/unit/config/annotations/conditions.py
@@ -7,6 +7,9 @@ from pydnameth import Observables
 from pydnameth import Cells
 from pydnameth import Attributes
 from pydnameth import Config
+from pydnameth.infrastucture.load.excluded import load_excluded
+from pydnameth.config.annotations.types import AnnotationKey
+from pydnameth.config.annotations.conditions import exclude_condition
 
 
 class TestAnnotationsConditions(unittest.TestCase):
@@ -26,7 +29,7 @@ class TestAnnotationsConditions(unittest.TestCase):
 
         annotations = Annotations(
             name='annotations',
-            exclude='none',
+            exclude='excluded',
             cross_reactive='ex',
             snp='ex',
             chr='NS',
@@ -60,7 +63,12 @@ class TestAnnotationsConditions(unittest.TestCase):
             is_root=True
         )
 
+    def test_exclude_condition(self):
+        self.config.excluded = load_excluded(self.config)
+        ex_cpg = {AnnotationKey.cpg.value: 'cg00000165'}
+        is_not_excluded = exclude_condition(self.config, ex_cpg)
+        self.assertEqual(False, is_not_excluded)
+
 
 if __name__ == '__main__':
-    pass
-
+    unittest.main()
