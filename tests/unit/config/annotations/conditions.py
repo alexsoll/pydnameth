391e5ae015c1e6d565a2477e29cc805851415e9a
Nikita Timakin <niktimak@gmail.com>
2019-02-28 13:09:53 +0300

    Annotations conditions tests file added

---
+++ tests/unit/config/annotations/conditions.py
@@ -0,0 +1,66 @@
+import unittest
+from tests.definitions import ROOT_DIR
+from pydnameth import Data
+from pydnameth import Experiment
+from pydnameth import Annotations
+from pydnameth import Observables
+from pydnameth import Cells
+from pydnameth import Attributes
+from pydnameth import Config
+
+
+class TestAnnotationsConditions(unittest.TestCase):
+    def setUp(self):
+        data = Data(
+            name='cpg_beta',
+            path=ROOT_DIR,
+            base='fixtures'
+        )
+
+        experiment = Experiment(
+            type=None,
+            task=None,
+            method=None,
+            params=None
+        )
+
+        annotations = Annotations(
+            name='annotations',
+            exclude='none',
+            cross_reactive='ex',
+            snp='ex',
+            chr='NS',
+            gene_region='yes',
+            geo='any',
+            probe_class='any'
+        )
+
+        observables = Observables(
+            name='observables',
+            types={}
+        )
+
+        cells = Cells(
+            name='cells',
+            types='any'
+        )
+
+        attributes = Attributes(
+            target='age',
+            observables=observables,
+            cells=cells
+        )
+
+        self.config = Config(
+            data=data,
+            experiment=experiment,
+            annotations=annotations,
+            attributes=attributes,
+            is_run=True,
+            is_root=True
+        )
+
+
+if __name__ == '__main__':
+    pass
+
