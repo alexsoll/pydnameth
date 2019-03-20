3dc9050b8354c604d35e3fe0184d492f3308ac8a
Unknown <hewitt.archie@yandex.ru>
2019-03-20 22:14:57 +0300

    Fixed cells subsetting in residual_common

--- pydnameth/scripts/develop.py
+++ pydnameth/scripts/develop.py
@@ -452,7 +452,8 @@ def cpg_proc_special_clock_linreg_dev(
         raise FileNotFoundError(f'File {file} not found.')
 
 
-def cpg_plot_methylation_scatter_dev(
+def plot_methylation_scatter_dev(
+    data_type,
     data,
     annotations,
     attributes,
@@ -466,7 +467,7 @@ def cpg_plot_methylation_scatter_dev(
         config_root = Config(
             data=copy.deepcopy(data),
             experiment=Experiment(
-                type=DataType.cpg,
+                type=data_type,
                 task=Task.methylation,
                 method=Method.scatter,
                 params=copy.deepcopy(params)
@@ -504,7 +505,7 @@ def cpg_plot_methylation_scatter_dev(
             config_child = Config(
                 data=copy.deepcopy(data),
                 experiment=Experiment(
-                    type=DataType.cpg,
+                    type=data_type,
                     task=Task.table,
                     method=copy.deepcopy(child_method),
                     params={}
@@ -518,6 +519,27 @@ def cpg_plot_methylation_scatter_dev(
 
         build_tree(root)
         calc_tree(root)
+
+
+def cpg_plot_methylation_scatter_dev(
+    data,
+    annotations,
+    attributes,
+    cpg_list,
+    observables_list,
+    child_method=Method.linreg,
+    params=None
+):
+    plot_methylation_scatter_dev(
+        DataType.cpg,
+        data,
+        annotations,
+        attributes,
+        cpg_list,
+        observables_list,
+        child_method,
+        params
+    )
 
 
 def residuals_common_plot_methylation_scatter_dev(
@@ -529,63 +551,37 @@ def residuals_common_plot_methylation_scatter_dev(
     child_method=Method.linreg,
     params=None
 ):
-    for cpg in cpg_list:
+    plot_methylation_scatter_dev(
+        DataType.residuals_common,
+        data,
+        annotations,
+        attributes,
+        cpg_list,
+        observables_list,
+        child_method,
+        params
+    )
 
-        config_root = Config(
-            data=copy.deepcopy(data),
-            experiment=Experiment(
-                type=DataType.residuals_common,
-                task=Task.methylation,
-                method=Method.scatter,
-                params=copy.deepcopy(params)
-            ),
-            annotations=copy.deepcopy(annotations),
-            attributes=copy.deepcopy(attributes),
-            is_run=True,
-            is_root=True
-        )
 
-        if config_root.experiment.params is None:
-            config_root.experiment.params = dict()
-
-        config_root.experiment.params['item'] = cpg
-
-        root = Node(name=str(config_root), config=config_root)
-
-        for d in observables_list:
-            observables_child = Observables(
-                name=copy.deepcopy(attributes.observables.name),
-                types=d
-            )
-
-            cells_child = Cells(
-                name=copy.deepcopy(attributes.cells.name),
-                types=copy.deepcopy(attributes.cells.types)
-            )
-
-            attributes_child = Attributes(
-                target=copy.deepcopy(attributes.target),
-                observables=observables_child,
-                cells=cells_child,
-            )
-
-            config_child = Config(
-                data=copy.deepcopy(data),
-                experiment=Experiment(
-                    type=DataType.residuals_common,
-                    task=Task.table,
-                    method=copy.deepcopy(child_method),
-                    params={}
-                ),
-                annotations=copy.deepcopy(annotations),
-                attributes=attributes_child,
-                is_run=False,
-                is_root=False
-            )
-            Node(name=str(config_child), config=config_child, parent=root)
-
-        build_tree(root)
-        calc_tree(root)
+def residuals_special_plot_methylation_scatter_dev(
+    data,
+    annotations,
+    attributes,
+    cpg_list,
+    observables_list,
+    child_method=Method.linreg,
+    params=None
+):
+    plot_methylation_scatter_dev(
+        DataType.residuals_special,
+        data,
+        annotations,
+        attributes,
+        cpg_list,
+        observables_list,
+        child_method,
+        params
+    )
 
 
 def cpg_plot_methylation_variance_histogram_dev(
